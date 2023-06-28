858a929364b1be3c8ec1d9d4a9759af31cc59bb5
wenhai0596 <wenhai_xm@qq.com>
2023-06-28 09:04:29 +0800

    Create camera_pins.h

---
+++ camera_pins.h
@@ -0,0 +1,18 @@
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM 45
+#define XCLK_GPIO_NUM 39
+#define SIOD_GPIO_NUM 21
+#define SIOC_GPIO_NUM 46
+
+#define Y2_GPIO_NUM 34
+#define Y3_GPIO_NUM 47
+#define Y4_GPIO_NUM 48
+#define Y5_GPIO_NUM 33
+#define Y6_GPIO_NUM 35
+#define Y7_GPIO_NUM 37
+#define Y8_GPIO_NUM 38
+#define Y9_GPIO_NUM 39
+
+#define VSYNC_GPIO_NUM 42
+#define HREF_GPIO_NUM 41
+#define PCLK_GPIO_NUM 36
